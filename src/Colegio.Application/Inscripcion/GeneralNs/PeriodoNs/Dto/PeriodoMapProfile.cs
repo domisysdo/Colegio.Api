76eb63f6d3a22dd7b95eb9a128a476e4db271295
José Polanco <josepolanco18@outlook.com>
2018-12-01 23:10:49 +0100

    Mergue

---
+++ src/Colegio.Application/Inscripcion/GeneralNs/PeriodoNs/Dto/PeriodoMapProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Colegio.Models.Inscripcion.GeneralNs.PeriodoNs;
+
+namespace Colegio.Incripcion.PeriodoNs.Dto
+{
+    public class PeriodoMapProfile: Profile
+    {
+        public PeriodoMapProfile()
+        {
+            CreateMap<PeriodoDto, Periodo>();
+            CreateMap<Periodo, PeriodoDto>();
+        }
+    }
+}
